3198a4d74b15ede402b2b8f04b2944ee0cb59648
djohnso88 <djohnso88@charlotttesvilleschools.org>
2022-09-30 13:49:51 -0400

    done

--- Launch4.py
+++ Launch4.py
@@ -1,6 +1,9 @@
 import time
 import board
 import digitalio
+import pwmio
+from adafruit_motor import servo
+
 led = digitalio.DigitalInOut(board.GP13)
 led.direction = digitalio.Direction.OUTPUT
 ledgreen = digitalio.DigitalInOut(board.GP18)
@@ -8,6 +11,9 @@ ledgreen.direction = digitalio.Direction.OUTPUT
 button=digitalio.DigitalInOut(board.GP28)
 button.direction = digitalio.Direction.INPUT 
 button.pull = digitalio.Pull.DOWN
+pwm_servo = pwmio.PWMOut(board.GP0, duty_cycle=2 ** 15, frequency=50)
+servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500)
+servo1.angle = 0
 
 for x in range(10,0,-1):
     print(x)
@@ -16,9 +22,9 @@ for x in range(10,0,-1):
     led.value = False
     time.sleep(.5)
 
+servo1.angle = 180
 print("lifftoff")
 ledgreen.value = True
 time.sleep(1)
 ledgreen.value = False
 time.sleep(1)
-button.pull = digitalio.PULL.UP
